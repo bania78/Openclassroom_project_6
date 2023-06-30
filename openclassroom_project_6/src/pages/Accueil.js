022fde11813f7785184c0319a63db0a2fc08dcdf
lou Wargnier <louis.bienvenue-wargnier@epitech.eu>
2023-06-30 22:03:54 +0200

    [FIX] index.scss

--- openclassroom_project_6/src/pages/Accueil.js
+++ openclassroom_project_6/src/pages/Accueil.js
@@ -3,6 +3,9 @@ import CardList from '../components/CardList/CardList';
 import img from '../assets/banner.jpg'
 
 function Accueil() {
+    let i = 1;
+    let f = (resolve => setTimeout(() => {resolve(i);}, 2000))
+    console.log(f);
     return (
         <div>
             <Banner img={img} title={"Chez vous, partout et ailleurs"}/>
